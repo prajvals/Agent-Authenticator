973627db505e1ca423392964d37f5732f3e32efe
prajvals <prajvaltheking@gmail.com>
2022-10-12 14:13:50 +0530

    initial commit

---
+++ Registration and Login Service/src/main/java/com/example/registrationandloginservice/RegistrationAndLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrationandloginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndLoginServiceApplication.class, args);
+	}
+
+}
---
+++ Registration and Login Service/src/test/java/com/example/registrationandloginservice/RegistrationAndLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationandloginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

39690b0357b0ad0681ab3c22bf97d13f4e82ab07
prajvals <prajvaltheking@gmail.com>
2022-10-13 12:33:51 +0530

    Created all basic classes

---
+++ Registration and Login Service/src/main/java/com/example/registrationandloginservice/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.registrationandloginservice.Repository;
+
+import com.example.registrationandloginservice.Entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users,Long> {
+}
---
+++ Registration and Login Service/src/main/java/com/example/registrationandloginservice/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.registrationandloginservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+}

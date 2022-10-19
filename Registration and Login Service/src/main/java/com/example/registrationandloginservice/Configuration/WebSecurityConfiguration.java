eefd824233a2fbf1a932ec4cbcff59cb2bdf298e
prajvals <prajvaltheking@gmail.com>
2022-10-19 13:24:54 +0530

    whitelisted apis to bypass spring secuirty

--- Registration and Login Service/src/main/java/com/example/registrationandloginservice/Configuration/WebSecurityConfiguration.java
+++ Registration and Login Service/src/main/java/com/example/registrationandloginservice/Configuration/WebSecurityConfiguration.java
@@ -1,15 +1,27 @@
 package com.example.registrationandloginservice.Configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration {
 
+    private static final String[] whiteListedAPIs= {
+            "/users",
+            "/CreateNewUser"
+    };
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeHttpRequests().antMatchers(whiteListedAPIs).permitAll();
+        return http.build();
     }
 }
